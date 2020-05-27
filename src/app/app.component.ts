0daf969ad16e1e140f74732ebeaa466a46302eda
Tarcizio Junior <tarcizio.junior2318@outlook.com>
2020-05-27 18:19:36 -0300

    Criando as funcoes para requisicao http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angularHttp';
+export class AppComponent implements OnInit{
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {}
+
+  onCreatePost(postData: {title: string; content: string}){
+    // Send HTTP request
+    this.http
+      .post(
+        'https://ng-complete-guide-9309e.firebaseio.com/',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  onFetchPosts(){}
+
+  onClearPosts(){}
+
 }
