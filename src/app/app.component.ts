a40bae360214e6e4eeaefdfc110b68a11b0c21e8
Tarcizio Junior <tarcizio.junior2318@outlook.com>
2020-05-28 14:17:54 -0300

    Criando o array para o recebimento de dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,9 @@ export class AppComponent implements OnInit{
   constructor(private http: HttpClient) {
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: {title: string; content: string}){
     // Send HTTP request
@@ -26,8 +29,29 @@ export class AppComponent implements OnInit{
       });
   }
 
-  onFetchPosts(){}
+  onFetchPosts(){
+    this.fetchPosts();
+  }
 
   onClearPosts(){}
+
+  private fetchPosts(){
+    this.http
+      .get('https://ng-complete-guide-9309e.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray
+              .push({ ...responseData[key], id: key });
+          }
+        }
+        return postsArray;
+        })
+      )
+      .subscribe(posts => {
+        console.log(posts);
+      });
+  }
 
 }
